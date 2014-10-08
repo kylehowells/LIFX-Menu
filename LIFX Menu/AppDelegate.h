216e5bd5859302555958a8155640269255809e7b
Kyle Howells <freerunnering@gmail.com>
2014-10-08 23:52:19 +0100

    Initial Commit

---
+++ LIFX Menu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LIFX Menu
+//
+//  Created by Kyle Howells on 08/10/2014.
+//  Copyright (c) 2014 Kyle Howells. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LIFX Menu/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LIFX Menu
+//
+//  Created by Kyle Howells on 08/10/2014.
+//  Copyright (c) 2014 Kyle Howells. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
